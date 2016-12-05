b96f0dda11a3a1657fad13060a4d758c659ce000
deirwin <deirwin@gmail.com>
2016-12-05 12:48:45 -0500

    Pushing some changes

--- Calendar.java
+++ Calendar.java
@@ -12,6 +12,9 @@ public class Calendar {
     public static void main(String[] args) {
 
         int month = Integer.parseInt(args[0]);    // month (Jan = 1, Dec = 12)	
+        if (month < 1 || month > 12) {
+          System.out.println("Month OutOfBoundsError");
+         }
         int year = Integer.parseInt(args[1]);     // year
 
         Print toScreen = new Print();
@@ -21,6 +24,13 @@ public class Calendar {
 	// get starting day
 	Day myday = new Day();
       	int d = myday.getDay(month, 1, year);
+if (d < 1 || d > 31) {
+  System.out.println("Day OutOfBoundsError");
+}
+
+
+
+
         toScreen.setStartDay(d);
 
         toScreen.printCalendar();
